d96d9a279d0833b6d7ee671812fb73e29969d1ea
judigot <judigot@gmail.com>
2024-06-19 19:00:00 +0800

    feat: improve styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useTransformationsStore } from '@/useTransformationsStore';
 import { frameworks, useFormStore } from '@/useFormStore';
-import '@/styles/style.css';
+import '@/styles/scss/main.scss';
 
 function App() {
   const { formData, setFormData } = useFormStore();
@@ -47,212 +47,270 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <div className="header">
-        <h1>App Scaffolder</h1>
+    <div className="text-white bg-black">
+      <div className="bg-gray-900 text-white p-4">
+        <h1 className="text-2xl font-bold">App Scaffolder</h1>
       </div>
+      <div className="grid gap-4 p-4">
+        {/* First row with form, create tables, and delete tables */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="bg-gray-800 p-4 shadow-md rounded-md">
+            <h2 className="text-xl font-bold mb-2">JSON Schema</h2>
+            <form className="space-y-4">
+              <textarea
+                id="schemaInput"
+                name="schemaInput"
+                value={formData.schemaInput}
+                onChange={handleChange}
+                rows={10}
+                className="mt-1 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+              />
+              <label
+                htmlFor="backendDir"
+                className="block text-sm font-medium text-gray-400"
+              >
+                Backend Directory:
+                <input
+                  type="text"
+                  id="backendDir"
+                  name="backendDir"
+                  value={formData.backendDir}
+                  onChange={handleChange}
+                  className="p-2 h-10 mt-1 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+                />
+              </label>
+              <label
+                htmlFor="frontendDir"
+                className="block text-sm font-medium text-gray-400"
+              >
+                Frontend Directory:
+                <input
+                  type="text"
+                  id="frontendDir"
+                  name="frontendDir"
+                  value={formData.frontendDir}
+                  onChange={handleChange}
+                  className="p-2 h-10 mt-1 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+                />
+              </label>
+              <label
+                htmlFor="dbConnection"
+                className="block text-sm font-medium text-gray-400"
+              >
+                Database Connection:
+                <input
+                  type="text"
+                  id="dbConnection"
+                  name="dbConnection"
+                  value={formData.dbConnection}
+                  onChange={handleChange}
+                  className="p-2 h-10 mt-1 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+                />
+              </label>
+              <label
+                htmlFor="framework"
+                className="block text-sm font-medium text-gray-400"
+              >
+                Framework:
+                <select
+                  id="framework"
+                  name="framework"
+                  value={formData.framework}
+                  onChange={handleChange}
+                  className="p-2 h-10 mt-1 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+                >
+                  <option value={''}>Select a framework</option>
+                  {Object.entries(frameworks).map(
+                    ([key, value]: [string, string]) => (
+                      <option key={key}>{value}</option>
+                    ),
+                  )}
+                </select>
+              </label>
+            </form>
+          </div>
 
-      <form className="form">
-        <label htmlFor="schemaInput" className="form-label">
-          Schema Input:
-          <textarea
-            id="schemaInput"
-            name="schemaInput"
-            value={formData.schemaInput}
-            onChange={handleChange}
-            rows={10}
-            className="form-textarea"
-          />
-        </label>
-        <label htmlFor="backendDir" className="form-label">
-          Backend Directory:
-          <input
-            type="text"
-            id="backendDir"
-            name="backendDir"
-            value={formData.backendDir}
-            onChange={handleChange}
-            className="form-input"
-          />
-        </label>
-        <label htmlFor="frontendDir" className="form-label">
-          Frontend Directory:
-          <input
-            type="text"
-            id="frontendDir"
-            name="frontendDir"
-            value={formData.frontendDir}
-            onChange={handleChange}
-            className="form-input"
-          />
-        </label>
-        <label htmlFor="dbConnection" className="form-label">
-          Database Connection:
-          <input
-            type="text"
-            id="dbConnection"
-            name="dbConnection"
-            value={formData.dbConnection}
-            onChange={handleChange}
-            className="form-input"
-          />
-        </label>
-        <label htmlFor="framework" className="form-label">
-          Framework:
-          <select
-            id="framework"
-            name="framework"
-            value={formData.framework}
-            onChange={handleChange}
-            className="form-select"
-          >
-            <option value={''}>Select a framework</option>
-
-            {Object.entries(frameworks).map(
-              ([key, value]: [string, string]) => (
-                <option key={key}>{value}</option>
-              ),
-            )}
-          </select>
-        </label>
-      </form>
-
-      {formData.schemaInput !== '' && formData.backendDir !== '' && (
-        <button
-          onClick={() => {
-            fetch(
-              `${String(import.meta.env.VITE_BACKEND_URL)}/${String(import.meta.env.VITE_API_URL)}/createFile`,
-              {
-                method: 'POST',
-                headers: {
-                  Accept: 'application/json',
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  targetDirectory: formData.backendDir,
-                  framework: formData.framework,
-                  content: 'Test Content',
-                }),
-              },
-            )
-              .then((response) => response.json())
-              .then(() => {
-                return;
-              })
-              .catch((error: unknown) => {
-                throw new Error(String(error));
-              });
-          }}
-        >
-          Generate Project Files
-        </button>
-      )}
-
-      <div className="columns">
-        <div className="column">
-          <h2>Database Schema</h2>
-          <label htmlFor="includeInsertData" className="form-checkbox-label">
-            <input
-              type="checkbox"
-              id="includeInsertData"
-              name="includeInsertData"
-              checked={includeInsertData}
-              onChange={handleChange}
-              className="form-checkbox"
+          <div className="bg-gray-800 p-4 shadow-md rounded-md">
+            <h2 className="text-xl font-bold mb-2">Create Tables</h2>
+            <textarea
+              id="SQLSchema"
+              value={SQLSchema}
+              readOnly
+              rows={15}
+              className="p-2 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
             />
-            Include Insert Data
-          </label>
-          <br />
-          <textarea id="SQLSchema" value={SQLSchema} readOnly rows={15} />
-          <button
-            onClick={() => {
-              handleCopy(SQLSchema);
-            }}
-            className="button"
-          >
-            Copy Database Schema
-          </button>
-          <h2>Delete Tables</h2>
-          <div className="join-queries">
-            {deleteTablesQueries.map((value, i) => (
-              <p key={i}>{value}</p>
-            ))}
-          </div>
-          <br />
-          <button
-            onClick={() => {
-              handleCopy(deleteTablesQueries.join('\n'));
-            }}
-            className="button"
-          >
-            Copy Delete Queries
-          </button>
-          &nbsp;
-          {formData.schemaInput !== '' && formData.dbConnection !== '' && (
+            <label
+              htmlFor="includeInsertData"
+              className="block text-sm font-medium text-gray-400 mt-4"
+            >
+              <input
+                type="checkbox"
+                id="includeInsertData"
+                name="includeInsertData"
+                checked={includeInsertData}
+                onChange={handleChange}
+                className="mr-2"
+              />
+              Include Insert Data
+            </label>
             <button
               onClick={() => {
                 handleCopy(SQLSchema);
               }}
-              className="button"
+              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
             >
-              Execute Query
+              Copy Database Schema
             </button>
-          )}
-          <h2>Join Queries</h2>
-          <div className="join-queries">
-            {joins.map((value, i) => (
-              <p key={i}>{value}</p>
-            ))}
-            {aggregateJoins.join('\n')}
+            <br />
+            {formData.schemaInput !== '' && formData.dbConnection !== '' && (
+              <button
+                onClick={() => {
+                  handleCopy(SQLSchema);
+                }}
+                className="mt-2 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+              >
+                Execute Query
+              </button>
+            )}
           </div>
-          <br />
-          <button
-            onClick={() => {
-              handleCopy(joins.join('\n'));
-            }}
-            className="button"
-          >
-            Copy Join Queries
-          </button>
-          &nbsp;
-          {formData.schemaInput !== '' && formData.dbConnection !== '' && (
+
+          <div className="bg-gray-800 p-4 shadow-md rounded-md">
+            <h2 className="text-xl font-bold mb-2">Delete Tables</h2>
+            <div className="block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 p-2">
+              {deleteTablesQueries.map((value, i) => (
+                <p key={i} className="whitespace-pre-wrap">
+                  {value}
+                </p>
+              ))}
+            </div>
             <button
               onClick={() => {
-                handleCopy(SQLSchema);
+                handleCopy(deleteTablesQueries.join('\n'));
               }}
-              className="button"
+              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
             >
-              Execute Query
+              Copy Delete Queries
             </button>
-          )}
+            <br />
+            {formData.schemaInput !== '' && formData.dbConnection !== '' && (
+              <button
+                onClick={() => {
+                  handleCopy(SQLSchema);
+                }}
+                className="mt-2 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+              >
+                Execute Query
+              </button>
+            )}
+          </div>
         </div>
 
-        <div className="column">
-          <h2>Interfaces</h2>
-          <textarea id="interfaces" value={interfaces} readOnly rows={10} />
-          <button
-            onClick={() => {
-              handleCopy(interfaces);
-            }}
-            className="button"
-          >
-            Copy Interfaces
-          </button>
-          <h2>Mock Data</h2>
-          <textarea
-            id="mockData"
-            value={JSON.stringify(mockData, null, 2)}
-            readOnly
-            rows={10}
-          />
-          <button
-            onClick={() => {
-              handleCopy(JSON.stringify(mockData, null, 2));
-            }}
-            className="button"
-          >
-            Copy Mock Data
-          </button>
+        {/* Second row with other content */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="bg-gray-800 p-4 shadow-md rounded-md">
+            <h2 className="text-xl font-bold mb-2">Join Queries</h2>
+            <div className="block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 p-2">
+              {joins.map((value, i) => (
+                <>
+                  <p key={i} className="whitespace-pre-wrap">
+                    {value}
+                  </p>
+                  <br />
+                </>
+              ))}
+            </div>
+            <button
+              onClick={() => {
+                handleCopy(joins.join('\n'));
+              }}
+              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            >
+              Copy Join Queries
+            </button>
+            <br />
+
+            {formData.schemaInput !== '' && formData.dbConnection !== '' && (
+              <button
+                onClick={() => {
+                  handleCopy(SQLSchema);
+                }}
+                className="mt-2 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+              >
+                Execute Query
+              </button>
+            )}
+          </div>
+
+          <div className="bg-gray-800 p-4 shadow-md rounded-md">
+            <h2 className="text-xl font-bold mb-2">Aggregate Join Queries</h2>
+            <div className="block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 p-2">
+              {aggregateJoins.map((value, i) => (
+                <>
+                  <p key={i} className="whitespace-pre-wrap">
+                    {value}
+                  </p>
+                  <br />
+                </>
+              ))}
+            </div>
+            <button
+              onClick={() => {
+                handleCopy(aggregateJoins.join('\n'));
+              }}
+              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            >
+              Copy Join Queries
+            </button>
+            <br />
+            {formData.schemaInput !== '' && formData.dbConnection !== '' && (
+              <button
+                onClick={() => {
+                  handleCopy(SQLSchema);
+                }}
+                className="mt-2 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+              >
+                Execute Query
+              </button>
+            )}
+          </div>
+
+          <div className="bg-gray-800 p-4 shadow-md rounded-md">
+            <h2 className="text-xl font-bold mb-2">Interfaces</h2>
+            <textarea
+              id="interfaces"
+              value={interfaces}
+              readOnly
+              rows={10}
+              className="p-2 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            />
+            <button
+              onClick={() => {
+                handleCopy(interfaces);
+              }}
+              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            >
+              Copy Interfaces
+            </button>
+          </div>
+
+          <div className="bg-gray-800 p-4 shadow-md rounded-md">
+            <h2 className="text-xl font-bold mb-2">Mock Data</h2>
+            <textarea
+              id="mockData"
+              value={JSON.stringify(mockData, null, 2)}
+              readOnly
+              rows={10}
+              className="p-2 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            />
+            <button
+              onClick={() => {
+                handleCopy(JSON.stringify(mockData, null, 2));
+              }}
+              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            >
+              Copy Mock Data
+            </button>
+          </div>
         </div>
       </div>
     </div>
