82d21cfc648e4ae81dc2d100f0a16672d778b46c
judigot <judigot@gmail.com>
2024-06-19 18:56:54 +0800

    feat: use incrementing primary keys and use actual foreign keys

--- src/utils/generateMockData.ts
+++ src/utils/generateMockData.ts
@@ -2,6 +2,11 @@ import { faker } from '@faker-js/faker';
 
 interface IFieldInfo {
   types: Set<string>;
+  isPrimaryKey?: boolean;
+  foreignKey?: {
+    table: string;
+    field: string;
+  };
 }
 
 const generateMockData = (
@@ -21,6 +26,21 @@ const generateMockData = (
       });
     });
 
+    // Identify primary keys
+    Object.keys(fieldInfo).forEach((key) => {
+      if (key.endsWith('_id')) {
+        fieldInfo[key].isPrimaryKey = true;
+      }
+    });
+
+    // Identify foreign keys
+    Object.entries(fieldInfo).forEach(([key, info]) => {
+      if (!(info.isPrimaryKey ?? false) && key.endsWith('_id')) {
+        const referencedTable = key.replace('_id', 's');
+        info.foreignKey = { table: referencedTable, field: key };
+      }
+    });
+
     const mockRecords = [];
 
     for (let i = 0; i < 10; i++) {
@@ -29,7 +49,18 @@ const generateMockData = (
       Object.entries(fieldInfo).forEach(([key, info]) => {
         const types = Array.from(info.types);
 
-        if (types.includes('string')) {
+        if (info.isPrimaryKey ?? false) {
+          mockRecord[key] = i + 1; // Generate ascending primary keys
+        } else if (info.foreignKey) {
+          const { table, field } = info.foreignKey;
+          const foreignRecords = generatedData[table] as Record<
+            string,
+            unknown
+          >[];
+          mockRecord[key] = faker.helpers.arrayElement(
+            foreignRecords.map((r) => r[field]),
+          );
+        } else if (types.includes('string')) {
           if (key.endsWith('_name')) {
             mockRecord[key] = faker.lorem.word();
           } else if (key.endsWith('_description')) {
