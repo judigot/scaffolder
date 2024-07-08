ca48a408ad1f6a697a862d969dfbab0372c7dc64
judigot <judigot@gmail.com>
2024-07-08 23:08:08 +0800

    refactor: improve generated data

--- src/utils/generateMockData.ts
+++ src/utils/generateMockData.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker';
+import convertType from './convertType';
 
 interface IFieldInfo {
   types: Set<string>;
@@ -48,9 +49,20 @@ const generateMockData = (
     for (let i = 0; i < MAX_ROWS; i++) {
       const mockRecord: Record<string, unknown> = {};
 
+      let firstName = '';
+      let lastName = '';
+
       Object.entries(fieldInfo).forEach(([rawColumnName, info]) => {
-        const types = Array.from(info.types);
         const columnName = rawColumnName.toLowerCase();
+        const sampleValue = records.find(
+          (record) => record[rawColumnName] !== null,
+        )?.[rawColumnName];
+
+        const fieldType = convertType({
+          primitiveType: [...info.types][0],
+          value: sampleValue,
+          targetType: 'typescript',
+        });
 
         if (info.isPrimaryKey ?? false) {
           mockRecord[rawColumnName] = i + 1; // Generate ascending primary keys
@@ -69,12 +81,51 @@ const generateMockData = (
           return;
         }
 
-        if (types.includes('string')) {
+        if (fieldType === 'string') {
+          if (columnName.includes('password')) {
+            mockRecord[rawColumnName] =
+              '$2b$10$M/WlJFeICXSTwvlM54X75u9Tg5Y3w/ak5T7O96cYY7mW0vJ2NFA7m';
+            return;
+          }
+          if (columnName.includes('email')) {
+            if (firstName && lastName) {
+              mockRecord[rawColumnName] = faker.internet
+                .email({
+                  firstName,
+                  lastName,
+                  allowSpecialCharacters: false,
+                  provider: 'example.com',
+                })
+                .toLowerCase();
+            } else {
+              mockRecord[rawColumnName] = faker.internet
+                .exampleEmail()
+                .toLowerCase();
+            }
+            return;
+          }
+          if (columnName.includes('username')) {
+            if (firstName && lastName) {
+              mockRecord[rawColumnName] = faker.internet
+                .userName({
+                  firstName,
+                  lastName,
+                })
+                .toLowerCase();
+            } else {
+              mockRecord[rawColumnName] = faker.internet
+                .userName()
+                .toLowerCase();
+            }
+            return;
+          }
           if (
             columnName.includes('first_name') ||
             columnName.includes('firstname')
           ) {
-            mockRecord[rawColumnName] = faker.person.firstName();
+            const generatedFirstName = faker.person.firstName();
+            mockRecord[rawColumnName] = generatedFirstName;
+            firstName = generatedFirstName;
             return;
           }
           if (
@@ -88,11 +139,17 @@ const generateMockData = (
             columnName.includes('last_name') ||
             columnName.includes('lastname')
           ) {
-            mockRecord[rawColumnName] = faker.person.lastName();
+            const generatedLastName = faker.person.lastName();
+            mockRecord[rawColumnName] = generatedLastName;
+            lastName = generatedLastName;
             return;
           }
           if (columnName.includes('_name')) {
-            mockRecord[rawColumnName] = faker.person.fullName();
+            const fullName = faker.person.fullName();
+            mockRecord[rawColumnName] = fullName;
+            const nameParts = fullName.split(' ');
+            firstName = nameParts[0];
+            lastName = nameParts[nameParts.length - 1];
             return;
           }
           if (columnName.endsWith('_description')) {
@@ -103,22 +160,18 @@ const generateMockData = (
           return;
         }
 
-        if (types.includes('number')) {
+        if (fieldType === 'number') {
           mockRecord[rawColumnName] = faker.number.int();
           return;
         }
 
-        if (types.includes('boolean')) {
+        if (fieldType === 'boolean') {
           mockRecord[rawColumnName] = faker.datatype.boolean();
           return;
         }
 
-        if (types.includes('object')) {
-          if (records.some((r) => r[rawColumnName] instanceof Date)) {
-            mockRecord[rawColumnName] = faker.date.past();
-          } else {
-            mockRecord[rawColumnName] = null;
-          }
+        if (fieldType === 'Date') {
+          mockRecord[rawColumnName] = faker.date.past();
           return;
         }
 
