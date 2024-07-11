5c3ca8a0af6b4c3aa551e704d7705faa8deb61ae
judigot <judigot@gmail.com>
2024-07-11 19:31:44 +0800

    refactor: improve symmetrical join algorithm

--- src/utils/identifyRelationships.ts
+++ src/utils/identifyRelationships.ts
@@ -5,69 +5,6 @@ export interface IRelationshipInfo {
   childTables: string[];
 }
 
-// function identifyRelationships(
-//   tableInfo: Record<string, Record<string, unknown>[]>,
-// ): {
-//   table: string;
-//   foreignTables: string[];
-//   foreignKeys: string[];
-// }[] {
-//   const relationships: IRelationshipInfo[] = [];
-//   const tableNames = Object.keys(tableInfo);
-
-//   tableNames.forEach((foreignTable) => {
-//     const foreignKeyColumns = Object.keys(tableInfo[foreignTable][0]).filter(
-//       (column) => column.endsWith('_id'),
-//     );
-
-//     const otherTables = tableNames.filter((table) => table !== foreignTable);
-
-//     otherTables.forEach((table) => {
-//       const tableColumns = tableInfo[table][0];
-
-//       foreignKeyColumns.forEach((foreignKey) => {
-//         const hasForeignKey = Object.prototype.hasOwnProperty.call(
-//           tableColumns,
-//           foreignKey,
-//         );
-
-//         if (hasForeignKey) {
-//           relationships.push({
-//             table,
-//             foreignTables: [foreignTable],
-//             foreignKeys: [foreignKey],
-//           });
-//         }
-//       });
-//     });
-//   });
-
-//   return mergeMultipleForeignKeys(relationships);
-
-//   function mergeMultipleForeignKeys(relationships: IRelationshipInfo[]) {
-//     const tableMap: Record<string, IRelationshipInfo> = {};
-//     const result: IRelationshipInfo[] = [];
-
-//     relationships.forEach((entry: IRelationshipInfo) => {
-//       const { table, foreignTables, foreignKeys } = entry;
-//       if (!(table in tableMap)) {
-//         tableMap[table] = { table, foreignTables: [], foreignKeys: [] };
-//       }
-//       tableMap[table].foreignTables.push(...foreignTables);
-//       tableMap[table].foreignKeys.push(...foreignKeys);
-//       result.push(entry);
-//     });
-
-//     Object.values(tableMap).forEach((combinedEntry: IRelationshipInfo) => {
-//       if (combinedEntry.foreignTables.length > 1) {
-//         result.push(combinedEntry);
-//       }
-//     });
-
-//     return result;
-//   }
-// }
-
 function identifyRelationships(
   data: Record<string, Record<string, unknown>[]>,
 ): IRelationshipInfo[] {
