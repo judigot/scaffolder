ddb07d4e683e0b4eea210e99a0cbc8ef950cfefd
judigot <judigot@gmail.com>
2024-07-12 16:49:29 +0800

    refactor: remove unnecessary conditions

--- src/utils/identifyType.ts
+++ src/utils/identifyType.ts
@@ -2,16 +2,12 @@ const identifyType = (value: unknown): string => {
   if (typeof value === 'number') {
     return Number.isInteger(value) ? 'number' : 'float';
   }
-  if (typeof value === 'string') {
-    return 'string';
-  }
-  if (typeof value === 'boolean') {
-    return 'boolean';
-  }
+
   if (value instanceof Date) {
     return 'Date';
   }
-  return 'unknown';
+
+  return typeof value;
 };
 
 export default identifyType;
