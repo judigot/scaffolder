a20ebbd67844f0763f345075b155a15ce85f18b1
judigot <judigot@gmail.com>
2024-08-09 11:44:37 +0800

    feat: ensure date values in insert queries adhere to db type

--- src/utils/generateSQLInserts.ts
+++ src/utils/generateSQLInserts.ts
@@ -1,10 +1,13 @@
 import { useFormStore } from '@/useFormStore';
+import { formatDateForMySQL } from '@/utils/common';
+import dayjs from 'dayjs';
 
 const generateSQLInserts = (
   data: Record<string, Record<string, unknown>[]>,
 ): string => {
   const quote = useFormStore.getState().quote;
   let inserts = '';
+  const { dbType } = useFormStore.getState();
 
   Object.entries(data).forEach(([tableName, records]) => {
     if (records.length === 0) {
@@ -14,26 +17,39 @@ const generateSQLInserts = (
     const firstRecord = records[0];
     const columnNames = Object.keys(firstRecord).join(', ');
 
-    const values = records
-      .map((record) => {
-        const valueStrings = Object.values(record).map((value) => {
-          if (typeof value === 'string') {
+    const values = records.map((record) => {
+      const mappedValues = Object.values(record).map((value) => {
+        if (typeof value === 'string') {
+          // Check if the string is a valid date
+          if (dayjs(value).isValid()) {
+            value = new Date(value);
+          } else {
             return `'${value.replace(/'/g, "''")}'`;
           }
-          if (value === null) {
-            return 'NULL';
+        }
+        if (value === null) {
+          return 'NULL';
+        }
+        if (
+          value instanceof Date ||
+          (typeof value === 'string' && dayjs(value).isValid())
+        ) {
+          const dateValue = value instanceof Date ? value : new Date(value);
+          if (dbType === 'postgresql') {
+            return `'${dateValue.toISOString()}'`;
           }
-          if (value instanceof Date) {
-            return `'${value.toISOString()}'`;
+
+          if (dbType === 'mysql') {
+            return `'${formatDateForMySQL(dateValue)}'`;
           }
-          return value;
-        });
+        }
+        return value;
+      });
 
-        return `(${valueStrings.join(', ')})`;
-      })
-      .join(',\n');
+      return `(${mappedValues.join(', ')})`;
+    });
 
-    inserts += `INSERT INTO ${quote}${tableName}${quote} (${columnNames}) VALUES ${values};\n`;
+    inserts += `INSERT INTO ${quote}${tableName}${quote} (${columnNames}) VALUES ${values.join()};\n`;
   });
 
   return inserts;
