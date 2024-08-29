5c599c78730f69441f7469a4ad6dca653d2cbc06
judigot <judigot@gmail.com>
2024-08-29 14:49:42 +0800

    refactor: use tablePlural

--- src/utils/createModels.ts
+++ src/utils/createModels.ts
@@ -6,7 +6,6 @@ import { IColumnInfo, ISchemaInfo } from '@/interfaces/interfaces';
 
 // Global variables
 const platform: string = process.platform;
-
 let __dirname = path.dirname(decodeURI(new URL(import.meta.url).pathname));
 if (platform === 'win32') {
   __dirname = __dirname.substring(1);
@@ -19,88 +18,111 @@ const getOwnerComment = (extension: string): string =>
     '.php': '/* Owner: App Scaffolder */\n',
   })[extension] ?? '/* Owner: App Scaffolder */\n';
 
-const createFillable = (
-  columnsInfo: IColumnInfo[],
+const filterAndMapColumns = (
+  columns: IColumnInfo[],
+  exemptions: string[],
   foreignKeys: string[],
-): string => {
-  const primaryKeyColumns = columnsInfo
-    .filter((column) => column.primary_key)
-    .map((column) => column.column_name);
-  const fillableColumns = columnsInfo
-    .filter(
-      (column) =>
-        !primaryKeyColumns.includes(column.column_name) &&
-        !fillableExemptions.includes(column.column_name),
-    )
+): string[] =>
+  columns
+    .filter((column) => !exemptions.includes(column.column_name))
     .map((column) => column.column_name)
     .concat(foreignKeys)
     .filter((value, index, self) => self.indexOf(value) === index); // Remove duplicates
 
-  return fillableColumns.map((column) => `'${column}'`).join(',\n        ');
-};
+const createFillable = (
+  columnsInfo: IColumnInfo[],
+  foreignKeys: string[],
+): string =>
+  filterAndMapColumns(
+    columnsInfo,
+    [
+      ...columnsInfo
+        .filter((col) => col.primary_key)
+        .map((col) => col.column_name),
+      ...fillableExemptions,
+    ],
+    foreignKeys,
+  )
+    .map((column) => `'${column}'`)
+    .join(',\n        ');
+
+const generateRelationMethods = (
+  relatedTables: string[],
+  callback: (table: string) => string,
+): string =>
+  relatedTables
+    .map((relatedTable) => callback(relatedTable))
+    .filter(Boolean)
+    .join('\n');
 
 const createRelationships = (
   tableName: string,
   foreignKeys: string[],
   hasOne: string[],
-  // hasMany: string[],
   belongsToMany: string[],
   tables: ISchemaInfo[],
 ): string => {
-  const parentPrimaryKey = tables
-    .find((table) => table.table === tableName)
-    ?.columnsInfo.find((column) => column.primary_key)?.column_name;
+  const getPrimaryKey = (table: string) =>
+    tables
+      .find((tbl) => tbl.table === table)
+      ?.columnsInfo.find((col) => col.primary_key)?.column_name;
 
-  const belongsToRelations = foreignKeys
-    .map((foreignKey) => {
-      const relationshipName = foreignKey.replace('_id', '');
-      return `    public function ${relationshipName}()\n    {\n        return $this->belongsTo(${toPascalCase(relationshipName)}::class, '${foreignKey}');\n    }\n`;
-    })
-    .join('\n');
+  const parentPrimaryKey = getPrimaryKey(tableName);
 
-  // Generate hasMany relationships based on the hasMany array and pivotRelationships
-  const hasManyRelations = tables
-    .find((table) => table.table === tableName)
-    ?.hasMany.filter((relatedTable) => {
-      const relatedTableInfo = tables.find(
-        (table) => table.table === relatedTable,
-      );
-      return (
-        (relatedTableInfo?.pivotRelationships.some(
-          (pivot) => pivot.relatedTable === tableName,
-        ) ??
-          false) ||
-        !(relatedTableInfo?.isPivot ?? false)
-      );
-    })
-    .map((relatedTable) => {
-      const childPrimaryKey = tables
-        .find((table) => table.table === relatedTable)
-        ?.columnsInfo.find((column) => column.primary_key)?.column_name;
+  const belongsToRelations = generateRelationMethods(
+    foreignKeys,
+    (foreignKey) =>
+      `    public function ${foreignKey.replace('_id', '')}()\n    {\n        return $this->belongsTo(${toPascalCase(
+        foreignKey.replace('_id', ''),
+      )}::class, '${foreignKey}');\n    }\n`,
+  );
 
-      if (childPrimaryKey != null && parentPrimaryKey != null) {
-        return `    public function ${relatedTable}s()\n    {\n        return $this->hasMany(${toPascalCase(relatedTable)}::class, '${parentPrimaryKey}');\n    }\n`;
-      }
-      return '';
-    })
-    .join('\n');
+  const hasManyRelations = generateRelationMethods(
+    tables
+      .find((table) => table.table === tableName)
+      ?.hasMany.filter((relatedTable) => {
+        const relatedTableInfo = tables.find(
+          (table) => table.table === relatedTable,
+        );
+        return (
+          relatedTableInfo?.pivotRelationships.some(
+            (pivot) => pivot.relatedTable === tableName,
+          ) ?? !(relatedTableInfo?.isPivot ?? false)
+        );
+      }) ?? [],
+    (relatedTable) => {
+      const relatedTablePlural = tables.find(
+        (tbl) => tbl.table === relatedTable,
+      )?.tablePlural;
+      const childPrimaryKey = getPrimaryKey(relatedTable);
 
-  const hasOneRelations = hasOne
-    .map((relatedTable) => {
+      return childPrimaryKey != null && parentPrimaryKey != null
+        ? `    public function ${String(relatedTablePlural)}()\n    {\n        return $this->hasMany(${toPascalCase(
+            relatedTable,
+          )}::class, '${parentPrimaryKey}');\n    }\n`
+        : '';
+    },
+  );
+
+  const hasOneRelations = generateRelationMethods(
+    hasOne,
+    (relatedTable) =>
+      `    public function ${relatedTable}()\n    {\n        return $this->hasOne(${toPascalCase(
+        relatedTable,
+      )}::class, '${String(parentPrimaryKey)}');\n    }\n`,
+  );
+
+  const belongsToManyRelations = generateRelationMethods(
+    belongsToMany,
+    (relatedTable) => {
       const relatedTableClass = toPascalCase(relatedTable);
-      return `    public function ${relatedTable}()\n    {\n        return $this->hasOne(${relatedTableClass}::class, '${String(parentPrimaryKey)}');\n    }\n`;
-    })
-    .join('\n');
-
-  const belongsToManyRelations = belongsToMany
-    .map((relatedTable) => {
-      const relatedTableClass = toPascalCase(relatedTable);
-
-      // Find the junction table that references both the current table and the related table
+      const relatedTablePlural = tables.find(
+        (tbl) => tbl.table === relatedTable,
+      )?.tablePlural;
       const junctionTable = tables.find(
-        (table) =>
-          table.foreignTables.includes(relatedTable) &&
-          table.foreignTables.includes(tableName),
+        (tbl) =>
+          tbl.foreignTables.includes(relatedTable) &&
+          tbl.foreignTables.includes(tableName),
       )?.table;
 
       if (junctionTable == null) {
@@ -109,17 +131,16 @@ const createRelationships = (
         );
       }
 
-      // Find the foreign keys in the junction table
       const currentTableForeignKey = `${tableName}_id`;
       const relatedTableForeignKey = `${relatedTable}_id`;
 
-      return `    public function ${relatedTable}s()\n    {\n        return $this->belongsToMany(${relatedTableClass}::class, '${junctionTable}', '${currentTableForeignKey}', '${relatedTableForeignKey}');\n    }\n`;
-    })
-    .join('\n');
+      return `    public function ${String(relatedTablePlural)}()\n    {\n        return $this->belongsToMany(${relatedTableClass}::class, '${junctionTable}', '${currentTableForeignKey}', '${relatedTableForeignKey}');\n    }\n`;
+    },
+  );
 
   return [
     belongsToRelations,
-    hasManyRelations, // Generate hasMany relationships correctly
+    hasManyRelations,
     hasOneRelations,
     belongsToManyRelations,
   ]
@@ -140,12 +161,8 @@ const generateModelImports = (
     ...belongsToMany,
     ...foreignKeys.map((fk) => fk.replace('_id', '')),
   ];
-  const uniqueRelatedTables = Array.from(new Set(relatedTables));
-
-  // Sort the table names alphabetically
-  uniqueRelatedTables.sort((a, b) => a.localeCompare(b));
-
-  return uniqueRelatedTables
+  return Array.from(new Set(relatedTables))
+    .sort((a, b) => a.localeCompare(b))
     .map((table) => `use App\\Models\\${toPascalCase(table)};`)
     .join('\n');
 };
@@ -192,7 +209,6 @@ const createModels = (
         table,
         foreignKeys,
         hasOne,
-        // hasMany,
         belongsToMany,
         schemaInfo,
       );
@@ -210,20 +226,23 @@ const createModels = (
         foreignKeys,
       );
 
-      const replacements = {
+      const model = createModelFile(template, {
         className,
         tableName: table,
         fillable,
         relationships,
         primaryKey,
         modelImports,
-      };
-      const model = createModelFile(template, replacements);
-      const ownerComment = getOwnerComment('.php');
-      const modelWithComment = model.replace('<?php', `<?php\n${ownerComment}`);
+      });
 
-      const outputFilePath = path.join(outputDir, `${className}.php`);
-      fs.writeFileSync(outputFilePath, modelWithComment);
+      const modelWithComment = model.replace(
+        '<?php',
+        `<?php\n${getOwnerComment('.php')}`,
+      );
+      fs.writeFileSync(
+        path.join(outputDir, `${className}.php`),
+        modelWithComment,
+      );
     },
   );
 };
