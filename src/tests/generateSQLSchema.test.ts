f092d0a3130cc2b5bb1c97917386cd6250aa4ada
judigot <judigot@gmail.com>
2024-08-30 10:18:02 +0800

    test: add more test cases for different schemas

--- src/tests/generateSQLSchema.test.ts
+++ src/tests/generateSQLSchema.test.ts
@@ -1,16 +1,21 @@
 import identifySchema from '@/utils/identifySchema';
 import { describe, it, expect } from 'vitest';
-import generateTypescriptInterfaces from '@/utils/generateTypescriptInterfaces';
 import generateSQLDeleteTables from '@/utils/generateSQLDeleteTables';
 import generateSQLSchema from '@/utils/generateSQLSchema';
 import { format as formatSQL } from 'sql-formatter';
 import { usersPostOneToOneSchema } from '@/json-schemas/usersPostOneToOneSchema';
+import { usersPostsOneToManySchema } from '@/json-schemas/usersPostsOneToManySchema';
+import { POSSchema } from '@/json-schemas/POSSchema';
 import { APP_SETTINGS } from '@/constants';
 
-describe('generateFile', () => {
+describe('generateSQLSchema', () => {
   const userPostOneToOneSchemaInfo = identifySchema(usersPostOneToOneSchema);
+  const userPostsOneToManySchemaInfo = identifySchema(
+    usersPostsOneToManySchema,
+  );
+  const POSSchemaInfo = identifySchema(POSSchema);
 
-  it('should generate correct SQL schema for one-to-one relationship with ON DELETE CASCADE', () => {
+  it('should generate correct SQL schema for one-to-one relationship', () => {
     const deleteTablesQueries = generateSQLDeleteTables(
       userPostOneToOneSchemaInfo,
     );
@@ -46,27 +51,62 @@ describe('generateFile', () => {
     }
   });
 
-  it('should generate correct TypeScript interfaces', () => {
-    const tsInterfaces = generateTypescriptInterfaces({
-      schemaInfo: userPostOneToOneSchemaInfo,
-      includeTypeGuards: true,
-      outputOnSingleFile: true,
-    });
-    expect(tsInterfaces).toContain('export interface IUser {');
-    expect(tsInterfaces).toContain('user_id: number;');
-    expect(tsInterfaces).toContain('first_name: string;');
-    expect(tsInterfaces).toContain('last_name: string;');
-    expect(tsInterfaces).toContain('email: string;');
-    expect(tsInterfaces).toContain('username: string;');
-    expect(tsInterfaces).toContain('password: string;');
-    expect(tsInterfaces).toContain('created_at: Date;');
-    expect(tsInterfaces).toContain('updated_at: Date;');
-    expect(tsInterfaces).toContain('export interface IPost {');
-    expect(tsInterfaces).toContain('post_id: number;');
-    expect(tsInterfaces).toContain('user_id: number;'); // Matches the one-to-one relationship
-    expect(tsInterfaces).toContain('title: string;');
-    expect(tsInterfaces).toContain('content: string | null;');
-    expect(tsInterfaces).toContain('created_at: Date;');
-    expect(tsInterfaces).toContain('updated_at: Date;');
+  it('should generate correct SQL schema for one-to-many relationship', () => {
+    const deleteTablesQueries = generateSQLDeleteTables(
+      userPostsOneToManySchemaInfo,
+    );
+    const sqlSchema = `${String(deleteTablesQueries.join('\n'))}\n\n${formatSQL(generateSQLSchema(userPostsOneToManySchemaInfo))}`;
+    expect(sqlSchema).toContain('DROP TABLE IF EXISTS "user";');
+    expect(sqlSchema).toContain('CREATE TABLE "user" (');
+    expect(sqlSchema).toContain('"user_id" BIGSERIAL PRIMARY KEY');
+    expect(sqlSchema).toContain('"first_name" TEXT NOT NULL');
+    expect(sqlSchema).toContain('"last_name" TEXT NOT NULL');
+    expect(sqlSchema).toContain('"email" TEXT UNIQUE NOT NULL');
+    expect(sqlSchema).toContain('"username" TEXT UNIQUE NOT NULL');
+    expect(sqlSchema).toContain('"password" CHAR(60) NOT NULL');
+    expect(sqlSchema).toContain('"created_at" TIMESTAMPTZ (6) NOT NULL');
+    expect(sqlSchema).toContain('"updated_at" TIMESTAMPTZ (6) NOT NULL');
+    expect(sqlSchema).toContain('DROP TABLE IF EXISTS "post";');
+    expect(sqlSchema).toContain('CREATE TABLE "post" (');
+    expect(sqlSchema).toContain('"post_id" BIGSERIAL PRIMARY KEY');
+    expect(sqlSchema).toContain('"user_id" BIGINT NOT NULL'); // One-to-many does not use UNIQUE constraint
+    expect(sqlSchema).toContain('"title" TEXT NOT NULL');
+    expect(sqlSchema).toContain('"content" TEXT');
+    expect(sqlSchema).toContain('"created_at" TIMESTAMPTZ (6) NOT NULL');
+    expect(sqlSchema).toContain('"updated_at" TIMESTAMPTZ (6) NOT NULL');
+    expect(sqlSchema).toContain(
+      'CONSTRAINT "FK_post_user_id" FOREIGN KEY ("user_id") REFERENCES "user" ("user_id")',
+    );
+  });
+
+  it('should generate correct SQL schema for POS', () => {
+    const deleteTablesQueries = generateSQLDeleteTables(POSSchemaInfo);
+    const sqlSchema = `${String(deleteTablesQueries.join('\n'))}\n\n${formatSQL(generateSQLSchema(POSSchemaInfo))}`;
+    expect(sqlSchema).toContain('DROP TABLE IF EXISTS "order_product";');
+    expect(sqlSchema).toContain('DROP TABLE IF EXISTS "order";');
+    expect(sqlSchema).toContain('DROP TABLE IF EXISTS "customer";');
+    expect(sqlSchema).toContain('DROP TABLE IF EXISTS "product";');
+    expect(sqlSchema).toContain('CREATE TABLE "product" (');
+    expect(sqlSchema).toContain('"product_id" BIGSERIAL PRIMARY KEY');
+    expect(sqlSchema).toContain('"product_name" TEXT NOT NULL');
+    expect(sqlSchema).toContain('CREATE TABLE "customer" (');
+    expect(sqlSchema).toContain('"customer_id" BIGSERIAL PRIMARY KEY');
+    expect(sqlSchema).toContain('"name" TEXT NOT NULL');
+    expect(sqlSchema).toContain('CREATE TABLE "order" (');
+    expect(sqlSchema).toContain('"order_id" BIGSERIAL PRIMARY KEY');
+    expect(sqlSchema).toContain('"customer_id" BIGINT NOT NULL');
+    expect(sqlSchema).toContain(
+      'CONSTRAINT "FK_order_customer_id" FOREIGN KEY ("customer_id") REFERENCES "customer" ("customer_id")',
+    );
+    expect(sqlSchema).toContain('CREATE TABLE "order_product" (');
+    expect(sqlSchema).toContain('"order_product_id" BIGSERIAL PRIMARY KEY');
+    expect(sqlSchema).toContain('"order_id" BIGINT NOT NULL');
+    expect(sqlSchema).toContain('"product_id" BIGINT NOT NULL');
+    expect(sqlSchema).toContain(
+      'CONSTRAINT "FK_order_product_order_id" FOREIGN KEY ("order_id") REFERENCES "order" ("order_id")',
+    );
+    expect(sqlSchema).toContain(
+      'CONSTRAINT "FK_order_product_product_id" FOREIGN KEY ("product_id") REFERENCES "product" ("product_id")',
+    );
   });
 });
