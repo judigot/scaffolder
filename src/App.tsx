01185560f7c941261f608e0c3417d38bd4ceb189
judigot <judigot@gmail.com>
2024-10-02 11:01:57 +0800

    feat: use introspected schema's schemaInfo

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import '@/styles/scss/main.scss';
 import { useModalStore } from '@/useModalStore';
 
 import { ISchemaInfo, isISchemaInfoArray } from '@/interfaces/interfaces';
-import generateMockData from '@/utils/generateMockData';
 import AdditionalSchemaSettings from '@/components/AdditionalSchemaSettings';
 
 function App() {
@@ -242,15 +241,54 @@ function App() {
                         /* prettier-ignore */ (() => { const QuickLog = schemaInfo; const isObject = (obj: unknown): obj is Record<string, unknown> => { return obj !== null && typeof obj === 'object'; }; const isArrayOfObjects = (arr: unknown): arr is Record<string, unknown>[] => { return Array.isArray(arr) && arr.every(isObject); }; const parentDiv: HTMLElement = document.getElementById('quicklogContainer') ?? (() => { const div = document.createElement('div'); div.id = 'quicklogContainer'; div.style.cssText = 'position: fixed; top: 10px; right: 10px; z-index: 1000; display: flex; flex-direction: column; align-items: flex-end; justify-content: space-between; max-height: 90vh; overflow-y: auto; padding: 10px; box-sizing: border-box;'; const helperButtonsDiv = document.createElement('div'); helperButtonsDiv.style.cssText = 'position: sticky; bottom: 0; display: flex; flex-direction: column; z-index: 1001;'; const clearButton = document.createElement('button'); clearButton.textContent = 'Clear'; clearButton.style.cssText = 'margin-top: 10px; background-color: red; color: white; border: none; padding: 5px; cursor: pointer; border-radius: 5px;'; clearButton.onclick = () => { if (parentDiv instanceof HTMLElement) { parentDiv.remove(); } }; helperButtonsDiv.appendChild(clearButton); document.body.appendChild(div); div.appendChild(helperButtonsDiv); return div; })(); const createTable = (obj: Record<string, unknown>): HTMLTableElement => { const table = document.createElement('table'); table.style.cssText = 'border-collapse: collapse; background-color: yellow; box-shadow: white 0px 0px 5px 1px; padding: 5px; border: 3px solid black; border-radius: 10px; color: black !important; cursor: pointer; font: bold 25px "Comic Sans MS"; margin-bottom: 10px;'; Object.entries(obj).forEach(([key, value]) => { const row = document.createElement('tr'); const keyCell = document.createElement('td'); const valueCell = document.createElement('td'); keyCell.textContent = key; valueCell.textContent = String(value); keyCell.style.cssText = 'border: 1px solid black; padding: 5px;'; valueCell.style.cssText = 'border: 1px solid black; padding: 5px;'; row.appendChild(keyCell); row.appendChild(valueCell); table.appendChild(row); }); return table; }; const createTableFromArray = ( arr: Record<string, unknown>[], ): HTMLTableElement => { const table = document.createElement('table'); table.style.cssText = 'border-collapse: collapse; background-color: yellow; box-shadow: white 0px 0px 5px 1px; padding: 5px; border: 3px solid black; border-radius: 10px; color: black !important; cursor: pointer; font: bold 25px "Comic Sans MS"; margin-bottom: 10px;'; const headers = Object.keys(arr[0]); const headerRow = document.createElement('tr'); headers.forEach((header) => { const th = document.createElement('th'); th.textContent = header; th.style.cssText = 'border: 1px solid black; padding: 5px;'; headerRow.appendChild(th); }); table.appendChild(headerRow); arr.forEach((obj) => { const row = document.createElement('tr'); headers.forEach((header) => { const td = document.createElement('td'); td.textContent = String(obj[header]); td.style.cssText = 'border: 1px solid black; padding: 5px;'; row.appendChild(td); }); table.appendChild(row); }); return table; }; const createChildDiv = (data: unknown): HTMLElement => { const newDiv = document.createElement('div'); const jsonData = JSON.stringify(data, null, 2); if (isArrayOfObjects(data)) { const table = createTableFromArray(data); newDiv.appendChild(table); } else if (isObject(data)) { const table = createTable(data); newDiv.appendChild(table); } else { newDiv.textContent = String(data); } newDiv.style.cssText = 'font: bold 25px "Comic Sans MS"; width: max-content; max-width: 500px; word-wrap: break-word; background-color: yellow; box-shadow: white 0px 0px 5px 1px; padding: 5px; border: 3px solid black; border-radius: 10px; color: black !important; cursor: pointer; margin-bottom: 10px;'; const handleMouseDown = (e: MouseEvent) => { e.preventDefault(); const clickedDiv = e.target instanceof Element && e.target.closest('div'); if (clickedDiv !== null && e.button === 0 && clickedDiv === newDiv) { void navigator.clipboard.writeText(jsonData).then(() => { clickedDiv.style.backgroundColor = 'gold'; setTimeout(() => { clickedDiv.style.backgroundColor = 'yellow'; }, 1000); }); } }; const handleRightClick = (e: MouseEvent) => { e.preventDefault(); if (parentDiv.contains(newDiv)) { parentDiv.removeChild(newDiv); if (!parentDiv.hasChildNodes()) { parentDiv.remove(); } } }; newDiv.addEventListener('mousedown', handleMouseDown); newDiv.addEventListener('contextmenu', handleRightClick); return newDiv; }; parentDiv.prepend(createChildDiv(QuickLog)); })();
 
                         if (isISchemaInfoArray(schemaInfo)) {
-                          const mockData = generateMockData({
-                            mockDataRows: 5,
-                            schemaInfo,
-                          });
-                          const newFormData = {
-                            ...useFormStore.getState().formData,
-                            schemaInput: JSON.stringify(mockData, null, 4),
-                          };
-                          setFormData(newFormData);
+                          // const mockData = generateMockData({
+                          //   mockDataRows: 5,
+                          //   schemaInfo,
+                          // });
+                          // const newFormData = {
+                          //   ...useFormStore.getState().formData,
+                          //   schemaInput: JSON.stringify(mockData, null, 4),
+                          // };
+                          // setFormData(newFormData);
+                          setTransformations(schemaInfo);
+
+                          setIsLoading(true);
+                          fetch(`http://localhost:5000/scaffoldProject`, {
+                            method: 'POST',
+                            headers: {
+                              Accept: 'application/json',
+                              'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                              schemaInfo,
+                              interfaces,
+                              backendDir,
+                              frontendDir,
+                              dbConnection,
+                              framework,
+                              SQLSchema,
+                            }),
+                          })
+                            .then((response) => response.json())
+                            .then((result: typeof generationStatus) => {
+                              // Success
+                              setGenerationStatus(result);
+                            })
+                            .catch((error: unknown) => {
+                              // Failure
+                              if (typeof error === `string`) {
+                                throw Error(`There was an error: error`);
+                              }
+                              if (error instanceof Error) {
+                                throw Error(
+                                  `There was an error: ${error.message}`,
+                                );
+                              }
+                            })
+                            .finally(() => {
+                              setIsLoading(false);
+                            });
+
                           setGenerationStatus({
                             ...generationStatus,
                             ...{
