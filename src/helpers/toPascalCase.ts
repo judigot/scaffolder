f83d1c4e083f7021f7edf88bb10a5bf322747ad6
judigot <judigot@gmail.com>
2024-10-02 10:58:57 +0800

    feat: add snake to camel case

--- src/helpers/toPascalCase.ts
+++ src/helpers/toPascalCase.ts
@@ -5,6 +5,19 @@ export const toPascalCase = (str: string): string => {
     .join('');
 };
 
+export const snakeToCamelCase = (input: string): string => {
+  return input
+    .split('_') // Split the string by underscores
+    .map((word, index) => {
+      // Capitalize the first letter of each word except the first one
+      if (index === 0) {
+        return word; // First word remains lowercase
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(''); // Join the words back together
+};
+
 export const convertToUrlFormat = (str: string): string => {
   return str.replace(/_/g, '-');
 };
