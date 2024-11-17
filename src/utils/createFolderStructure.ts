f93d6baf63316dd83fa3f90f0a271bfe0a6e3492
judigot <judigot@gmail.com>
2024-11-17 19:09:13 +0800

    feat: integrate folder cleanup before creating new structure

--- src/utils/createFolderStructure.ts
+++ src/utils/createFolderStructure.ts
@@ -1,9 +1,10 @@
 import { IStructure } from '@/components/FileViewer';
+import clearGeneratedFiles from '@/utils/clearDirectory';
 import fs from 'fs';
 import path from 'path';
 
 /**
- * Recursively creates files and folders from the given structure.
+ * Recursively creates files and folders from the given structure after cleaning up old files.
  *
  * @param params - The parameters object containing structure and targetDirectory.
  */
@@ -17,6 +18,11 @@ export const createFolderStructure = ({
   structure.forEach((item) => {
     if (item.type === 'folder') {
       const folderPath = path.join(targetDirectory, item.name);
+
+      /* Clear old files before creating a new folder structure */
+      if (fs.existsSync(folderPath)) {
+        clearGeneratedFiles(folderPath);
+      }
 
       if (!fs.existsSync(folderPath)) {
         fs.mkdirSync(folderPath, { recursive: true });
