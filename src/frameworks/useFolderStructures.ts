72bf941475ddb280015d4fa38747f21990356026
judigot <judigot@gmail.com>
2024-11-19 18:21:16 +0800

    style: rename imports

--- src/frameworks/useFolderStructures.ts
+++ src/frameworks/useFolderStructures.ts
@@ -1,14 +1,14 @@
 import { IStructure } from '@/components/FileViewer';
 import { ISchemaInfo } from '@/interfaces/interfaces';
 import { frameworks } from '@/useFormStore';
-import { useLaravel as laravelFolderStructure } from '@/utils/backend/laravel/useLaravel';
-import { useNextJS as nextjsFolderStructure } from '@/utils/backend/nextjs/useNextJS';
+import { useLaravel } from '@/utils/backend/laravel/useLaravel';
+import { useNextJS } from '@/utils/backend/nextjs/useNextJS';
 
 export function useFolderStructures(
   schemaInfo: ISchemaInfo[],
 ): Record<string, IStructure> {
   return {
-    [frameworks.LARAVEL]: laravelFolderStructure({ schemaInfo }),
-    [frameworks.NEXTJS]: nextjsFolderStructure({ schemaInfo }),
+    [frameworks.LARAVEL]: useLaravel({ schemaInfo }),
+    [frameworks.NEXTJS]: useNextJS({ schemaInfo }),
   };
 }
