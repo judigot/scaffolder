10c453e4c64391d3b2d3de5a3ea3e783ebd43bc2
judigot <judigot@gmail.com>
2024-12-09 17:02:17 +0800

    chore: remove unused templates

--- src/templates/frontend/api/model/delete.ts
+++
@@ -1,7 +0,0 @@
-import { customFetch } from '../customFetch';
-
-export const deleteModelTemplate = async (id: number): Promise<void> => {
-  await customFetch.delete({
-    url: `/modelTemplate/${String(id)}`,
-  });
-};
--- src/templates/frontend/interfaces/interfaces.ts
+++
@@ -1,3 +0,0 @@
-export interface IModelTemplate {
-  id: number;
-}
