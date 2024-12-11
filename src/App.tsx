2c472fd2686bf8b543d735172599c9c7418d3d43
judigot <judigot@gmail.com>
2024-12-11 23:53:17 +0800

    feat: improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -629,11 +629,28 @@ function App() {
           </div>
 
           <div className="bg-gray-800 p-4 shadow-md rounded-md">
+            <h2 className="text-xl font-bold mb-2">Mock Data</h2>
+            <textarea
+              id="mockData"
+              value={JSON.stringify(mockData, null, 2)}
+              readOnly
+              rows={10}
+              className="p-2 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            />
+            <button
+              onClick={() => {
+                handleCopy(JSON.stringify(mockData, null, 2));
+              }}
+              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            >
+              Copy Mock Data
+            </button>
+          </div>
+
+          <div className="bg-gray-800 p-4 shadow-md rounded-md">
             {directJoins.length > 0 && (
               <>
-                <h2 className="text-xl font-bold mb-2">
-                  Direct Join Queries
-                </h2>
+                <h2 className="text-xl font-bold mb-2">Direct Join Queries</h2>
                 <div className="block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 p-2">
                   {directJoins.map((value, i) => (
                     <div key={i}>
@@ -702,25 +719,6 @@ function App() {
                 </button>
               </>
             )}
-          </div>
-
-          <div className="bg-gray-800 p-4 shadow-md rounded-md">
-            <h2 className="text-xl font-bold mb-2">Mock Data</h2>
-            <textarea
-              id="mockData"
-              value={JSON.stringify(mockData, null, 2)}
-              readOnly
-              rows={10}
-              className="p-2 block w-full border border-gray-700 bg-gray-900 text-white rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
-            />
-            <button
-              onClick={() => {
-                handleCopy(JSON.stringify(mockData, null, 2));
-              }}
-              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
-            >
-              Copy Mock Data
-            </button>
           </div>
 
           <div className="bg-gray-800 p-4 shadow-md rounded-md">
