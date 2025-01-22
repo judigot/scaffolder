5cc0d9d333b875c2262719407616aa6a5365d6d9
judigot <judigot@gmail.com>
2025-01-22 12:06:12 +0800

    chore: update project configuration and dependencies
    - Changed nodeModulesDir in deno.json from 'none' to 'auto' to allow automatic management of node modules.
    - Added new dependencies including 'concurrently' for improved script execution in package.json.
    - Updated deno.lock and pnpm-lock.yaml to reflect the latest versions of dependencies, ensuring compatibility and performance enhancements.
    - Refactored main.tsx to uncomment and integrate key components for better application structure and functionality.

--- src/main.tsx
+++ src/main.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '@/styles/scss/main.scss';
-// import App from '@/App.tsx';
-// import SQLSchemaInputModal from '@/components/SQLSchemaInputModal.tsx';
-// import TransformationTester from '@/TransformationTester.tsx';
-// import ModalProvider from '@/components/Modal/base/ModalProvider.tsx';
+import App from '@/App.tsx';
+import SQLSchemaInputModal from '@/components/SQLSchemaInputModal.tsx';
+import TransformationTester from '@/TransformationTester.tsx';
+import ModalProvider from '@/components/Modal/base/ModalProvider.tsx';
 
-import { FormParser } from '@/dynamic-form/ReactFormParser.tsx';
-import { JSONFormStructure } from '@/dynamic-form/DynamicFormStructure.ts';
+// import { FormParser } from '@/dynamic-form/ReactFormParser.tsx';
+// import { JSONFormStructure } from '@/dynamic-form/DynamicFormStructure.ts';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <FormParser structure={JSONFormStructure} />
+      {/* <FormParser structure={JSONFormStructure} /> */}
 
-      {/* <ModalProvider />
+      <ModalProvider />
       <SQLSchemaInputModal />
       <TransformationTester />
-      <App /> */}
+      <App />
     </React.StrictMode>,
   );
 }
