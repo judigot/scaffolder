5b83a3b3484e9e01b4a1bf9f10956e81236b0634
judigot <judigot@gmail.com>
2025-03-24 21:39:14 +0800

    feat: increase request body size limit for JSON and URL-encoded data

--- src/index.ts
+++ src/index.ts
@@ -19,9 +19,11 @@ if (platform === 'win32') {
 const publicDirectory = path.join(__dirname, 'public');
 
 // Enable middleware
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 app.use(express.static(publicDirectory));
+
 
 // Define routes
 app.get('/', (_req: Request, res: Response) => {
