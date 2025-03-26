07b33bcbaf4a56a4d983bec1bafd14eeba20c15b
judigot <judigot@gmail.com>
2025-03-26 10:46:28 +0800

    refactor: remove relatedFiles from ICommandOptions interface

--- src/utils/project-builder/interfaces/interfaces.ts
+++ src/utils/project-builder/interfaces/interfaces.ts
@@ -4,7 +4,6 @@ export type Replacements = Record<string, ReplacementValue>;
 export interface ICommandOptions {
   conditions?: string[];
   template?: string;
-  relatedFiles?: string[];
   includeTable?: string;
   scoped?: boolean;
   excludeTable?: string;
