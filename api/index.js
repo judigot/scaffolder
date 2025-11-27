97950c75a5b9b5af5fc62b32cbddea91fcad8a65
judigot <judigot@gmail.com>
2025-11-27 13:10:20 +0800

    docs: add important placeholder index.js for Vercel API recognition

---
+++ api/index.js
@@ -0,0 +1,11 @@
+/**
+ * ⚠️  IMPORTANT: Do not remove this file
+ *
+ * This file is required by Vercel to recognize the api/ directory as a serverless function.
+ *
+ * During the build process (pnpm build), this placeholder will be automatically
+ * overwritten with the actual bundled Express server code. However, Vercel needs
+ * this file to exist in the repository to properly configure the API route.
+ *
+ * @see package.json - "build:api" script for build details
+ */
