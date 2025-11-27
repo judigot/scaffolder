f5cd2582e8de164ebd4dc65fb37f0c35fd61c934
judigot <judigot@gmail.com>
2025-11-27 12:50:23 +0800

    feat: revert to working version

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -3,7 +3,7 @@ import { Router, type Request, type Response } from 'express';
 const router = Router();
 
 router.get('/hello', (_req: Request, res: Response) => {
-  return res.json({ message: 'Hello, world!' });
+  return res.json({ message: 'Hello, world changed!' });
 });
 
 export default router;
