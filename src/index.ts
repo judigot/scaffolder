3a49425753cb648edcbc24c13a69a79cafa56792
judigot <judigot@gmail.com>
2025-11-27 11:51:50 +0800

    chore: improve development environment

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ import router from '@/routes/index.ts';
 dotenv.config();
 
 const app = express();
-const VITE_BACKEND_PORT = (process.env.VITE_BACKEND_PORT ?? 5000).toString();
+const isProduction: boolean = process.env.NODE_ENV === 'production';
+const PORT = process.env.VITE_BACKEND_PORT ?? '5000';
 const platform: string = process.platform;
 let __dirname = path.dirname(decodeURI(new URL(import.meta.url).pathname));
 
@@ -17,7 +18,9 @@ if (platform === 'win32') {
   __dirname = __dirname.substring(1);
 }
 
-const publicDirectory = path.join(__dirname, 'public');
+const publicDirectory = isProduction
+  ? path.join(__dirname, '..', 'dist')
+  : path.join(__dirname, 'public');
 
 // Enable middleware
 app.use(compression());
@@ -26,30 +29,31 @@ app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 app.use(express.static(publicDirectory));
 
-// Define routes
-app.get('/', (_req: Request, res: Response) => {
-  const isDevelopment: boolean = String(process.env.NODE_ENV) === 'development';
-
-  if (isDevelopment) {
-    res.redirect(String(process.env.VITE_FRONTEND_URL));
-    return;
-  }
-
-  res.sendFile(publicDirectory);
-});
-
 // Use routes from the routes folder
 app.use('/api', router);
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send({ message: 'AlienStack' });
-});
+if (isProduction) {
+  app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(publicDirectory, 'index.html'));
+  });
+} else {
+  app.get('/', (_req: Request, res: Response) => {
+    const isDevelopment: boolean = process.env.NODE_ENV === 'development';
+
+    if (!isDevelopment) {
+      res.redirect(process.env.VITE_FRONTEND_URL ?? '');
+      return;
+    }
+
+    res.send({ message: 'AlienStack' });
+  });
+}
 
 // Start server
-app.listen(VITE_BACKEND_PORT, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(
-    `${platform.charAt(0).toUpperCase() + platform.slice(1)} is running on ${String(process.env.VITE_BACKEND_HOST)}:${VITE_BACKEND_PORT}`,
+    `${platform.charAt(0).toUpperCase() + platform.slice(1)} is running on ${process.env.VITE_BACKEND_HOST ?? 'localhost'}:${PORT}`,
   );
 });
 
