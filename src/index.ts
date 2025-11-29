2270cf1b883831492a1fcebec57ba1610ef979d8
judigot <judigot@gmail.com>
2025-11-29 15:30:06 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { Hono } from 'hono';
 import { compress } from 'hono/compress';
-// import { bodyLimit } from 'hono/body-limit';
-// import { cors } from 'hono/cors';
+import { bodyLimit } from 'hono/body-limit';
+import { cors } from 'hono/cors';
 import { serveStatic } from '@hono/node-server/serve-static';
 import indexRouter from '@/app/routes/index.ts';
 
 const app = new Hono();
 
 app.use('/api', compress());
-// app.use('*', bodyLimit({ maxSize: 100 * 1024 * 1024 }));
-// app.use('*', cors());
+app.use('/api', bodyLimit({ maxSize: 100 * 1024 * 1024 }));
+app.use('/api', cors());
 
 app.route('/api', indexRouter);
 
