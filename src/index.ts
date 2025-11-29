9ff92b0a32888399c845255c225f126686b09f5a
judigot <judigot@gmail.com>
2025-11-29 15:06:04 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
 import { Hono } from 'hono';
-// import { compress } from 'hono/compress';
+import { compress } from 'hono/compress';
 // import { bodyLimit } from 'hono/body-limit';
 // import { cors } from 'hono/cors';
-// import { serveStatic } from '@hono/node-server/serve-static';
+import { serveStatic } from '@hono/node-server/serve-static';
 import indexRouter from '@/app/routes/index.ts';
 
 const app = new Hono();
 
-// app.use('*', compress());
+app.use('*', compress());
 // app.use('*', bodyLimit({ maxSize: 100 * 1024 * 1024 }));
 // app.use('*', cors());
 
 app.route('/api', indexRouter);
 
-// if (process.env.VERCEL !== '1') {
-//   if (process.env.NODE_ENV !== 'development') {
-//     app.get('*', serveStatic({ root: 'dist' }));
-//   } else {
-//     app.get('*', (c) => {
-//       const port = String(process.env.VITE_FRONTEND_PORT);
-//       const url = `http://localhost:${port}${c.req.path}`;
-//       return c.redirect(url, 302);
-//     });
-//   }
-// }
+if (process.env.VERCEL !== '1') {
+  if (process.env.NODE_ENV !== 'development') {
+    app.get('*', serveStatic({ root: 'dist' }));
+  } else {
+    app.get('*', (c) => {
+      const port = String(process.env.VITE_FRONTEND_PORT);
+      const url = `http://localhost:${port}${c.req.path}`;
+      return c.redirect(url, 302);
+    });
+  }
+}
 
-// const hono = { port: Number(process.env.VITE_BACKEND_PORT), fetch: app.fetch };
+const hono = { port: Number(process.env.VITE_BACKEND_PORT), fetch: app.fetch };
 
-// if (process.env.VERCEL !== '1') {
-//   hono.port = Number(process.env.VITE_BACKEND_PORT);
-// }
+if (process.env.VERCEL !== '1') {
+  hono.port = Number(process.env.VITE_BACKEND_PORT);
+}
 
-// export default hono;
-export default app;
+export default hono;
+// export default app;
