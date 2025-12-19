d6c3c39107d4945f9845851330ccab11a1241787
judigot <judigot@gmail.com>
2025-12-19 16:16:18 +0800

    chore: add new applications and update TypeScript configuration
    - Added configurations for two new applications: @bigbang/app1 and @bigbang/app2 in bun.lock, including their dependencies and devDependencies.
    - Updated tsconfig.app.json to include the 'apps' directory in the TypeScript compilation.

---
+++ apps/app1/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'node:path';
+
+export default defineConfig({
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  server: {
+    host: true,
+    port: 3001,
+  },
+  plugins: [react(), tsconfigPaths()],
+});
