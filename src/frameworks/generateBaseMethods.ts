bfd008fde399894fe2d76c81a3ad7075d4d48eec
judigot <judigot@gmail.com>
2026-01-08 16:40:54 +0800

    refactor: update method directory resolution in multiple files
    - Changed the method directory resolution from using `path.dirname(new URL(import.meta.url).pathname)` to `__dirname` in various files across the project. This adjustment ensures consistent path resolution, improving compatibility and reliability in file operations.

--- src/frameworks/generateBaseMethods.ts
+++ src/frameworks/generateBaseMethods.ts
@@ -84,7 +84,7 @@ const generateFeatureFiles = (operation: IRepositoryStructure[]) => {
 import fs from 'fs';
 import path from 'path';
 
-const currentDir = path.dirname(new URL(import.meta.url).pathname);
+const currentDir = __dirname;
 
 export default {
   methodName: fs.readFileSync(path.join(currentDir, 'methodName.txt'), 'utf8'),
