ddc7002f5562bdf2b702a74d3db9834a5df2b62d
Jude Francis <judigot@gmail.com>
2026-02-05 11:32:09 +0000

    feat(infra): add HTTP/HTTPS ports to EC2 security group

--- infra/ubuntu-ec2/main.tf
+++ infra/ubuntu-ec2/main.tf
@@ -59,6 +59,20 @@ resource "aws_security_group" "ec2" {
     cidr_blocks = var.ssh_allowed_cidrs
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
